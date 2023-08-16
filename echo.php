151df10a2571d54fe52654fceb3e03100a08fdb3
urumci <nijatt23@gmail.com>
2023-08-16 13:23:22 +0300

    php

---
+++ echo.php
@@ -0,0 +1,37 @@
+<?php 
+echo "merhaba paylastikca aylesi";
+echo "<br>";
+echo "nasilsiniz?";
+echo "<br>";
+echo "herkese selamlar";
+echo "Bende iyim tesekur ederim";
+echo "<br>";
+echo 27;
+echo "<br>";
+echo "27";
+
+echo "<br>";
+echo 'merhaba kymetli\' doslarim';
+
+echo "<br>";
+echo 'merhaba kymetli" doslarim';
+
+echo "<br>";
+echo "merhaba kymetli\" doslarim";
+
+echo "<br>";
+echo ("merhaba arkadaslar");
+echo (288);
+
+?>
+
+<b>kalin icerek</b> <!-- bu kisimi html-->
+<!--   php komutlari
+    baslangic  |            |     bitis
+    "                              "
+    '                              '
+    "               '              "
+    '                "             '
+    "                \"            "
+    '                \'            '  
+-->
