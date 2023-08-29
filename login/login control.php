8bc37277db1df2574d25f4ab7d9ec158fa111244
urumci <nijatt23@gmail.com>
2023-08-29 11:10:11 +0300

    29.8 1

---
+++ login/login control.php
@@ -0,0 +1,11 @@
+<?php
+
+// echo "user name :" .$_POST['username'];
+// echo "<br> password: " .$_POST['password'];
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+echo $username."<br>". $password;
+
+
+?>
